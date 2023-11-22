b368f8157c97e6597c505f650ee8ac9094c11f9f
epitchi <thienvanlea1@gmail.com>
2023-11-22 16:49:50 +0700

    + experiment steamcmd cli

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.20
 
 require github.com/UserExistsError/conpty v0.1.1
 
-require golang.org/x/sys v0.13.0 // indirect
+require (
+	github.com/jensvandewiel/gosteamcmd v0.1.2
+	golang.org/x/sys v0.13.0 // indirect
+)
--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"os/exec"
-	"strconv"
 	"time"
+
+	"github.com/jensvandewiel/gosteamcmd"
+	"github.com/jensvandewiel/gosteamcmd/console"
+
 )
 
 type SteamConfig struct {
@@ -25,36 +27,61 @@ type Stores struct {
 }
 
 func main(){
-
-	gamePath := "c:\\GameServer\\"
-
-
 	fmt.Println("Callback Virtless change")
 	// - SQL Query to Virtless, check lastest app_id -> if yes, execute, if not, wait for 12 hours
-	gamesNeedDownload := supabaseCheck("virtless.thinkmay.net", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNjk0MDE5NjAwLAogICJleHAiOiAxODUxODcyNDAwCn0.EpUhNso-BMFvAJLjYbomIddyFfN--u-zCf0Swj9Ac6E")
+	// gamesNeedDownload := supabaseCheck("virtless.thinkmay.net", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNjk0MDE5NjAwLAogICJleHAiOiAxODUxODcyNDAwCn0.EpUhNso-BMFvAJLjYbomIddyFfN--u-zCf0Swj9Ac6E")
 
 
 
-	for _, p := range gamesNeedDownload {
-		fmt.Println(p)
+	// for _, p := range gamesNeedDownload {
+	// 	fmt.Println(p)
 
-		foundSteamApp := findSteamInfo(p.Name)
-		if foundSteamApp != nil {
-			fmt.Println(foundSteamApp)
-			steamDownload := exec.Command("steamcmd", 
-				"+force_install_dir", 	gamePath + "json[app_id].app_name", 
-				"+login", 				"username",  "password", 
-				"+app_update", 			strconv.Itoa(foundSteamApp.AppId), 
-				"+quit")
-			
-			fmt.Println(steamDownload)
-		}
-	}
+	// 	foundSteamApp := findSteamInfo(p.Name)
+	// 	if foundSteamApp != nil {
+	// 		fmt.Println(foundSteamApp)
+	// 		// steamDownload := exec.Command("steamcmd", 
+	// 		// 	"+force_install_dir", 	gamePath + "json[app_id].app_name", 
+	// 		// 	"+login", 				"username",  "password", 
+	// 		// 	"+app_update", 			strconv.Itoa(foundSteamApp.AppId), 
+	// 		// 	"+quit")
+	// 	}
+	// }
 
+
+
+	steamcmd := SteamCmdExec(2430930, "rust_server", "", "")
+	fmt.Println(steamcmd)
 
 
 	fmt.Println("Zip and Ship")
 
+}
+
+func SteamCmdExec(steam_id int, steam_name string, username string, pwd string) *string {
+	fmt.Println("hehe")
+
+	prompts := []*gosteamcmd.Prompt{
+		gosteamcmd.ForceInstallDir("/home/epitchi/steamcmd/" + steam_name),
+		gosteamcmd.Login(username, pwd, ""),
+		gosteamcmd.AppUpdate(steam_id, "", true),
+	}
+
+
+	cmd := gosteamcmd.New(os.Stdout, prompts, "/usr/games/steamcmd")
+
+	cmd.Console.Parser.OnInformationReceived = func(action console.Action, progress float64, currentWritten, total uint64) {
+		println("")
+	}
+	cmd.Console.Parser.OnAppInstalled = func(app uint32) {
+		println("App installed: ", app, " Yay!")
+	}
+
+	_, err := cmd.Run()
+
+	if err != nil {
+		panic(err)
+	}
+	return nil
 }
 
 

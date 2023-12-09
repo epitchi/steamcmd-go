12492d27fb5334eedc7cb277458f8803063a5eb4
epitchi <thienvanlea1@gmail.com>
2023-12-09 16:46:31 +0700

    + restrucutre zipfodler, steamcmd, main

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
+	"reflect"
 	"strings"
 
-	"github.com/epitchi/steamcmdgo/utils"
+	"github.com/epitchi/steamcmdgo/utils/fileserver"
+	"github.com/epitchi/steamcmdgo/utils/steamcmd"
+	"github.com/epitchi/steamcmdgo/utils/supabase"
+	"github.com/epitchi/steamcmdgo/utils/zipfolder"
 )
 
 type SteamConfig struct {
@@ -16,27 +21,39 @@ type SteamConfig struct {
 	Password string `json:"password"`
 }
 
-
 func main() {
-	fmt.Println("Callback Virtless change")
 	// - SQL Query to Virtless, check lastest app_id -> if yes, execute, if not, wait for 12 hours
-	gamesNeedDownload := utils.SupabaseCheck("virtless.thinkmay.net", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNjk0MDE5NjAwLAogICJleHAiOiAxODUxODcyNDAwCn0.EpUhNso-BMFvAJLjYbomIddyFfN--u-zCf0Swj9Ac6E")
+	gamesNeedDownload := supabase.SupabaseCheck("sontay.thinkmay.net", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNjk0MDE5NjAwLAogICJleHAiOiAxODUxODcyNDAwCn0.EpUhNso-BMFvAJLjYbomIddyFfN--u-zCf0Swj9Ac6E")
+
+	if reflect.DeepEqual(gamesNeedDownload, []supabase.Stores{}) {
+		fmt.Println("No found any game need to download")
+	}
 
 	for _, p := range gamesNeedDownload {
 		fmt.Println(p)
-
 		foundSteamApp := findSteamInfo(p.Name)
 		if foundSteamApp != nil {
 			fmt.Println(foundSteamApp)
+			result := steamcmd.SteamCmdExec(268910, "Cuphead", "USER_NAME", "PASSWORD")
+			
+			if result != nil {
+				zipfolder.ZipFile(result.SourceDir, "/Games" + result.SteamName)
+			}
+
+			port := flag.Int("port", 8080, "Port to listen on")
+			directory := flag.String("dir", ".", "/Game")
+			flag.Parse()
+			err := fileserver.StartServer(*port, *directory)
+			if err != nil {
+				println("Error:", err)
+				os.Exit(1)
+			}
+
 		}
 	}
 
-	result := utils.SteamCmdExec(268910, "Cuphead", "", "")
 
 
-	if result != nil {
-		utils.ZipFile(result.SourceDir, result.SteamName)
-	}
 
 }
 
--- utils/steamcmd/steamcmd.go
+++ utils/steamcmd/steamcmd.go
@@ -1,4 +1,4 @@
-package utils
+package steamcmd
 
 import (
 	"os"
